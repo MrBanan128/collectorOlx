155cb96b7efbc0320b9bd3b12f65ee1892c2009c
Szymon Banaszek <szymon.banaszek128@onet.pl>
2025-02-11 17:26:13 +0100

    ShinyText, clear index.css

--- CollectorApp/src/components/layout/AppLink.jsx
+++ CollectorApp/src/components/layout/AppLink.jsx
@@ -2,6 +2,8 @@ import { Link } from '@chakra-ui/react';
 import { NavLink } from 'react-router';
 import { useLocation } from 'react-router';
 
+import ShinyText from '../ui/ShinyText/shinytext';
+
 // eslint-disable-next-line react/prop-types
 const AppLink = ({ children, to }) => {
   const location = useLocation();
@@ -15,7 +17,7 @@ const AppLink = ({ children, to }) => {
       _focus={{ outline: 'none' }}
       fontWeight={'bold'}
       fontSize={25}
-      color={active ? 'blue.700' : 'gray.300'}
+      color={active ? 'rgba(14, 7, 217, 0.67)' : 'rgba(222, 222, 224, 0.67)'}
       borderBottom={active ? '2px solid ' : 'none'}
       cursor={'pointer'}
       _hover={{
@@ -26,8 +28,15 @@ const AppLink = ({ children, to }) => {
       transition={'all ease-in-out 0.3s'}
       role="navigation"
     >
-      {children}
+      <ShinyText
+        text={children}
+        disabled={false}
+        speed={4}
+        color
+        className="ShinyNavLink"
+      />
     </Link>
   );
 };
+
 export default AppLink;
